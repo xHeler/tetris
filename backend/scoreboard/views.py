1d297151ecdc6b765d6b298e2283b7dd14903431
xHeler <kwiatkowsky.kontakt@gmail.com>
2022-08-24 17:17:06 +0200

    Add recently table

--- backend/scoreboard/views.py
+++ backend/scoreboard/views.py
@@ -8,3 +8,9 @@ class ScoreListView(ListView):
     model = Score
     context_object_name = 'score_list'
     template_name = 'scoreboard/score_list.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of recently 5 results
+        context['recently_results'] = Score.objects.all().order_by('-created_at')[:5]
+        return context
