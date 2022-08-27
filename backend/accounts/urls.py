ae49df806937a94aee2ef2133f24ca13647f3cc5
xHeler <kwiatkowsky.kontakt@gmail.com>
2022-08-27 15:13:21 +0200

    Add custom login page

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView
 
 from .views import SignupPageView
 
+
 urlpatterns = [
+    path('login/', LoginView.as_view(), name="login"),
     path("signup/", SignupPageView.as_view(), name="signup"),
 ]
 
